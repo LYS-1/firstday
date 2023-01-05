96fa775dfbdc858894d3b186cf6b3cac16ddf718
LYS-1 <skyduq123@gmail.com>
2023-01-05 10:40:11 +0900

    1

--- src/com/iu/s4/objects6/Robot.java
+++ src/com/iu/s4/objects6/Robot.java
@@ -1,14 +1,17 @@
 package com.iu.s4.objects6;
 
-public class Robot {
+public class Robot {//defendency 의존성
 	
 	private ArmRight armRight;
 	private ArmLeft armLeft;
 	
 	public Robot() {
-		this.armRight = new ArmRight();
+		this.armRight = new ArmRight();//defendency injection 의존성 주입
 	}
 	
+	public Robot(ArmRight armRight) {
+		this.armRight = armRight;
+	}
 	
 	public ArmRight getArmRight() {
 		return armRight;
